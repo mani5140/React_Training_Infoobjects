030fa28c1fa8462fc6500a5e2efa281c21648ad2
mani5140 <srimanikantagarapati12@gmail.com>
2024-06-10 18:28:01 +0530

    create first component

---
+++ src/usersData.tsx
@@ -0,0 +1,49 @@
+export type UserInterface = {
+    name: string;
+    age: number;
+    src: string;
+    details: string[];
+};
+
+export const usersData: UserInterface[] = [
+    {
+        name: 'mani',
+        age: 22,
+        src: "https://i.imgur.com/QIrZWGIs.jpg",
+        details: [
+            "he is a resident of odisha",
+            "he has completed btech from lpu",
+            "he is a software developer"
+        ]
+    },
+    {
+        name: 'anup',
+        age: 16,
+        src: "https://i.imgur.com/QIrZWGIs.jpg",
+        details: [
+            "he is a resident of odisha",
+            "he has completed btech from lpu",
+            "he is a software developer"
+        ]
+    },
+    {
+        name: 'pranay',
+        age: 24,
+        src: "https://i.imgur.com/QIrZWGIs.jpg",
+        details: [
+            "he is a resident of odisha",
+            "he has completed btech from lpu",
+            "he is a software developer"
+        ]
+    },
+    {
+        name: 'shikha',
+        age: 17,
+        src: "https://i.imgur.com/QIrZWGIs.jpg",
+        details: [
+            "he is a resident of odisha",
+            "he has completed btech from lpu",
+            "he is a software developer"
+        ]
+    }
+];
